30cf62ca10dc9dc4f89d1e7459e23d13833485e0
ko <go@marusoft.net>
2016-12-06 02:43:15 +0900

    Initial commit

---
+++ app/src/main/java/yesman/af/softwareengineeringdepartment/cbnu/cosmetic/DBStaticValue/RANKING.java
@@ -0,0 +1,11 @@
+package yesman.af.softwareengineeringdepartment.cbnu.cosmetic.DBStaticValue;
+
+public class RANKING {
+	public static String c = "cosmetic";	
+	public static String brand_name = c+"_brand_name";
+	public static String name = c+"_name";
+	public static String rank = c+"_rank";
+	public static String volume = c+"_volume";
+	public static String review_count = c+"_review_count";
+	public static String id = c+"_id";
+}
---
+++ app/src/main/java/yesman/af/softwareengineeringdepartment/cbnu/cosmetic/MainActivity.java
@@ -0,0 +1,61 @@
+package yesman.af.softwareengineeringdepartment.cbnu.cosmetic;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button get_cosmetic_btn;
+    Button get_all_cosmetic_btn;
+    Button get_rank_cosmetic_btn;
+    Button store_review_btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        get_cosmetic_btn = (Button) findViewById(R.id.get_cosmetic_btn);
+        get_all_cosmetic_btn = (Button) findViewById(R.id.get_all_cosmetic_btn);
+        get_rank_cosmetic_btn = (Button) findViewById(R.id.get_rank_cosmetic_btn);
+        store_review_btn = (Button) findViewById(R.id.store_review);
+
+        get_cosmetic_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ServerManager manager = ServerManager.getInstance();
+                manager.get_cosmetic_data();
+            }
+        });
+
+        get_all_cosmetic_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ServerManager manager = ServerManager.getInstance();
+                manager.getAllCosmetic_withReview();
+            }
+        });
+        get_rank_cosmetic_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ServerManager manager = ServerManager.getInstance();
+                manager.get_rank_cosmetic_info();
+            }
+        });
+
+        store_review_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ServerManager manager = ServerManager.getInstance();
+                manager.store_review(new Review("Lush","Dirty Spray","reviewr_id","좋아요!",3,4.0,1));
+            }
+        });
+
+
+    }
+
+
+
+}
